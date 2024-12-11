a9f09fb677d39086a4d9ecc68c5ffbd5b20d6645
tatiane <tatiane.andrade@wehandle.com.br>
2024-12-11 20:41:26 -0300

    initial commit

---
+++ src/main/java/com/poc/dbloader/PocDatabaseLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.poc.dbloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDatabaseLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDatabaseLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/dbloader/PocDatabaseLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.dbloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDatabaseLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
